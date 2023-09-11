4a3f827f44b90ceb25393090b83112472b29f153
pedlo07 <pedroalmeidaveber@gmail.com>
2023-09-11 20:06:38 -0300

    initial commit

---
+++ arrayTeste.js
@@ -0,0 +1,9 @@
+var arrayTeste = ['henrique', 'Mathias', 'Eduardo'];
+
+console.log(arrayTeste[0]);
+arrayTeste.unshift('Pedro');
+console.log(arrayTeste[0]);
+arrayTeste.push('Gustavo');
+console.log(arrayTeste[4]);
+arrayTeste.shift();
+console.log(arrayTeste);
