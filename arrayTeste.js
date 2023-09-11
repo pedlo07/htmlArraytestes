fe8e4796b6b1a6fa615b2d1ab042c55ac72ab0cf
pedlo07 <pedroalmeidaveber@gmail.com>
2023-09-11 20:16:07 -0300

    update

--- arrayTeste.js
+++ arrayTeste.js
@@ -7,3 +7,15 @@ arrayTeste.push('Gustavo');
 console.log(arrayTeste[4]);
 arrayTeste.shift();
 console.log(arrayTeste);
+
+// busca pelo indice
+console.log(arrayTeste[3]);
+
+// busca pelo elemento
+console.log(arrayTeste.indexOf('Mathias'));
+
+var array1 = ['Henrique', 'Mathias', 'Eduardo'];
+var array2 = ['Maria', 'Willan', 'Igor'];
+var array3 = array1.concat(array2);
+
+console.log(array3);
